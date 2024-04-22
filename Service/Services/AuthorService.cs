c36f0a1ad24cdb8bbadf38c80d4454d8e6bd8299
Vzart <vo.dang.khoa.main@gmail.com>
2024-04-22 09:06:46 +0700

    im stoopid

--- Service/Services/AuthorService.cs
+++ Service/Services/AuthorService.cs
@@ -91,6 +91,7 @@ public class AuthorService : IAuthorService
         author.Zip = request.Zip;
 
         var result = _authorRepository.Update(author);
+        await _uow.CommitAsync();
         return _mapper.Map<AuthorModel>(author);
     }
 
